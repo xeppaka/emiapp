de46b8ad8fdf76b45e434e4097cfd3bf17ebf0d6
Pavel Kachalouski <xeppaka@gmail.com>
2016-09-20 00:13:58 +0200

    Products menu, not collapsible yet.

--- ui/src/emi/components/menu/compitem.js
+++ ui/src/emi/components/menu/compitem.js
@@ -5,6 +5,7 @@ const CompItem = ({depth, text, children}) => {
     var style = {"padding-left": depth * 15};
     return (
         <div className="list-group" style={style}>
+            <a id="test" href="#test" className="list-group-item list-group-item-action" style={style} data-toggle="collapse">{text}</a>
             {
                 children.map(function(elem) {
                     if (elem.hasOwnProperty("children")) {
--- ui/src/emi/components/menu/productsmenu.js
+++ ui/src/emi/components/menu/productsmenu.js
@@ -1,15 +1,26 @@
 import React, { PropTypes } from 'react';
 import CompItem from './compitem';
 
-let menu = {
+const menu = {
     text: 'Test 1',
-    children: [],
+    children: [
+        {
+            text: 'test 2'
+        },
+        {
+            text: 'test 2'
+        },
+        {
+            text: 'test 2'
+        },
+        {
+            text: 'test 2'
+        }
+    ]
 };
 
 const ProductsMenu = () => {
-    return (
-        <CompItem depth={0} text={menu.text} children={menu.children} />
-    )
+    return <CompItem depth={0} text={menu.text} children={menu.children} />
 };
 
 export default ProductsMenu;
