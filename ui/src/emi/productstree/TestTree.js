f89d9d42cc2115802e286003135470aefa0941a4
Pavel Kachalouski <pavel.kachalouski@ca.com>
2016-09-20 18:10:01 +0200

    Implementing tree of products.

--- ui/src/emi/productstree/TestTree.js
+++ ui/src/emi/productstree/TestTree.js
@@ -41,9 +41,12 @@ class CategoryNode {
 class ProductsTree {
     constructor() {
         this.rootCategory = new CategoryNode('root');
+        this.productsList = null;
     }
 
     addProduct(categories, product) {
+        this.productsList = null;
+
         var categoriesArray = categories.split(':');
         var categoriesLength = categoriesArray.length;
         var currentCategoryNode = this.rootCategory;
@@ -54,6 +57,32 @@ class ProductsTree {
 
         currentCategoryNode.addProduct(product);
     }
+
+    getProducts() {
+        if (this.productsList === null) {
+            this.productsList = this.prepareProductsList(this.rootCategory);
+        }
+
+        return this.productsList;
+    }
+
+    prepareProductsList(category) {
+        var currProducts = category.getProducts();
+
+        var childCategories = category.getChildCategories();
+        var childCategoriesLength = childCategories.length;
+
+        for (var i = 0; i < childCategoriesLength; i++) {
+            currProducts = currProducts.concat(this.prepareProductsList(childCategories[i]));
+        }
+
+        return currProducts;
+    }
 }
 
 var t = new ProductsTree();
+t.addProduct('1:1:1', {name: 'product 1:1:1'});
+t.addProduct('1:2:2', {name: 'product 1:2:2'});
+t.addProduct('1:1:2', {name: 'product 1:1:2'});
+
+console.log(t.getProducts());
